2a589c55381e54890a3dad832cb0d197f59aea5c
James <james.ivie@gmail.com>
2026-01-21 19:58:55 -0700

    update to support string version put into AssemblyInformationalVersion as well

--- Program.cs
+++ Program.cs
@@ -2,26 +2,30 @@
 
 Regex VersionNumberValidate = new(@"^[0-9]+(\.[0-9]+){1,3}$", RegexOptions.Compiled);
 Regex ProjectFileVersionReplace = new(@"(?<=<(?:Assembly|File|Package|)Version>)[^<]*", RegexOptions.Compiled);
+Regex ProjectFileInformationalVersionReplace = new Regex(@"(?<=<InformationalVersion>)[^<]*", RegexOptions.Compiled);
 Regex PackageOutputPathReplace = new(@"<PackageOutputPath>[^<]*</PackageOutputPath>", RegexOptions.Compiled);
 Regex AssemblyInfoVersionReplace = new(@"(?<=\[.*Assembly.*Version ?\(\"")([^\""]*)", RegexOptions.Compiled);
+Regex AssemblyInformationalVersion = new Regex(@"(?<=\[.*Assembly.*Informational.*Version ?\(\"")([^\""]*)", RegexOptions.Compiled);
 
 if (args.Length < 1)
 {
     Console.WriteLine("Usage: ");
-    Console.WriteLine("\tdotnet-set-version <version_number>");
+    Console.WriteLine("\tdotnet-set-version <version_number> (<alternate_version_string>)");
     Console.WriteLine("");
     return;
 }
 
-string? version = null;
+string? versionNumber = null;
+string? alternateVersionString = null;
 bool noPackageOutputPath = false;
 foreach (string arg in args)
 {
-    if (String.Equals(arg, "-NoPackageOutputPath") || String.Equals(arg, "/NoPackageOutputPath")) noPackageOutputPath = true;
+    if (string.Equals(arg, "-NoPackageOutputPath") || string.Equals(arg, "/NoPackageOutputPath")) noPackageOutputPath = true;
     // check for a version number
-    if (VersionNumberValidate.IsMatch(arg)) version = arg;
+    if (VersionNumberValidate.IsMatch(arg)) versionNumber = arg;
+    else if (versionNumber == null) alternateVersionString = arg;
 }
-if (version == null)
+if (versionNumber == null)
 {
     Console.WriteLine("The specified version must be of the form nnn.nnn[.nnn[.nnn]]");
     Console.WriteLine("");
@@ -29,26 +33,36 @@ if (version == null)
 }
 foreach (string targetFile in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csproj", SearchOption.AllDirectories))
 {
-    UpdateProjectFile(targetFile, args[0], noPackageOutputPath);
+    UpdateProjectFile(targetFile, versionNumber, alternateVersionString, noPackageOutputPath);
 }
 foreach (string targetFile in Directory.GetFiles(Directory.GetCurrentDirectory(), "AssemblyInfo.cs", SearchOption.AllDirectories))
 {
-    UpdateCodeFile(targetFile, args[0]);
+    UpdateCodeFile(targetFile, versionNumber, alternateVersionString);
 }
 
-void UpdateProjectFile(string targetFile, string newVersion, bool noPackageOutputPath)
+void UpdateProjectFile(string targetFile, string newVersion, string? alternateVersionString, bool noPackageOutputPath)
 {
     string targetFileContents = File.ReadAllText(targetFile);
+    if (alternateVersionString != null)
+    {
+        targetFileContents = ProjectFileInformationalVersionReplace.Replace(targetFileContents, newVersion);
+    }
     targetFileContents = ProjectFileVersionReplace.Replace(targetFileContents, newVersion);
     if (noPackageOutputPath) targetFileContents = PackageOutputPathReplace.Replace(targetFileContents, "");
     File.WriteAllText(targetFile, targetFileContents);
-    Console.WriteLine($"Updated versions in {targetFile} to {newVersion}.");
+    string outputAlternateVersionSuffix = alternateVersionString != null ? $" and informational version {alternateVersionString}" : "";
+    Console.WriteLine($"Updated versions in {targetFile} to {newVersion}{outputAlternateVersionSuffix}.");
 }
 
-void UpdateCodeFile(string targetFile, string newVersion)
+void UpdateCodeFile(string targetFile, string newVersion, string? alternateVersionString)
 {
     string targetFileContents = File.ReadAllText(targetFile);
     targetFileContents = AssemblyInfoVersionReplace.Replace(targetFileContents, newVersion);
+    if (alternateVersionString != null)
+    {
+        targetFileContents = AssemblyInformationalVersion.Replace(targetFileContents, newVersion);
+    }
     File.WriteAllText(targetFile, targetFileContents);
-    Console.WriteLine($"Updated versions in {targetFile} to {newVersion}.");
+    string outputAlternateVersionSuffix = alternateVersionString != null ? $" and informational version {alternateVersionString}" : "";
+    Console.WriteLine($"Updated versions in {targetFile} to {newVersion}{outputAlternateVersionSuffix}.");
 }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [dotnet-set-version](https://github.com/jamesivie/dotnet-set-version) sets version numbers into project files and AssemblyInfo files.
 
 ## Usage:
-`dotnet-set-version <version_number>`
+`dotnet-set-version <version_number> (alt_version_string)`
 
 ## Global Installation
 
@@ -13,3 +13,10 @@
 `dotnet new tool-manifest # if you are setting up this repo`
 `dotnet tool install --local dotnet-set-version --version <tool_version>`
 
+## GitHub Action Sample Usage
+```
+      - name: Install dotnet-set-version
+        run: dotnet tool install --global dotnet-set-version
+      - name: Run dotnet-set-version
+        run: dotnet-set-version 0.1.${{ github.run_number }} ${{ github.sha }} -NoPackageOutputPath
+```
